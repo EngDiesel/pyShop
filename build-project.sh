ccf022da5e8ed66d7a392671303ecc6e43086c86
Mostafa Yasin <mostafa.ys2014@gmail.com>
2019-04-02 22:08:14 +0200

    Added static directory and maneged it's operations.

--- build-project.sh
+++ build-project.sh
@@ -1,2 +1,6 @@
+echo "Generating ../static/ folder"
+cp -r static/ ../static/
+echo "Please Enter 'yes' to collect static files"
+python2 manage.py collectstatic
 echo 'Installing requirements, this may take sevral minutes.'
 pip install -r requirements.txt
--- runserver.sh
+++ runserver.sh
@@ -1,1 +1,2 @@
-python2 manage.py runserver > logfile.log
+echo "Server Running on port:8000 go to http://127.0.0.1:8000/ to view the home page"
+python2 manage.py runserver
